97a5e9f3818f2a0004e87b1cdace34f26ce589f3
Kobayashi <kobayash@rcnp.osaka-u.ac.jp>
2026-02-16 16:57:10 +0900

    printer host updated

--- chkstatus.sh
+++ chkstatus.sh
@@ -9,16 +9,16 @@ cd ${rcnp_printers_dir}
 ./each_printer.sh a2c2570   
 ./each_printer.sh a2c3540   
 ./each_printer.sh a3c2570   
-./each_printer.sh ri1c2250  
+./each_printer.sh ri1c4000
 ./each_printer.sh rbc4150   
-./each_printer.sh rcc4000a 
-./each_printer.sh rcc4000b  
+./each_printer.sh rcc2570a 
+./each_printer.sh rcc2570b  
 ./each_printer.sh h1c3540   
 ./each_printer.sh h2c2570   
 ./each_printer.sh h3c2570   
 ./each_printer.sh h4c5570   
 ./each_printer.sh h5c2570   
 ./each_printer.sh h6c2570   
-./each_printer.sh g1c2250   
+./each_printer.sh g1c4000   
 
 cd ${pwd}
